b6189fd3cbccd867f83a273a0cd8cdbc5bf7eace
Anna Miroshnichenko <miroshnichenkoanna74@gmail.com>
2022-01-10 18:18:04 +0500

    Add Button for calling projectApi

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,14 +1,30 @@
+import React from 'react';
 import './ExploreContainer.css';
 
 interface ContainerProps {
   name: string;
 }
 
-const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
+const ExploreContainer: React.FC<ContainerProps> = ({ name, children }) => {
   return (
     <div className="container">
-      <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      {children ? (
+        children
+      ) : (
+        <React.Fragment>
+          <strong>{name}</strong>
+          <p>
+            Explore{' '}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://ionicframework.com/docs/components"
+            >
+              UI Components
+            </a>
+          </p>
+        </React.Fragment>
+      )}
     </div>
   );
 };
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -17,8 +17,9 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-        <Button/>
+        <ExploreContainer name="Tab 1 page" >
+           <Button/>
+        </ExploreContainer>
       </IonContent>
     </IonPage>
   );
